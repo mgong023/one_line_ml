e93bc1f48beded20282c365f6950409e35ec2e33
Marvin Ong <marvin_ong@outlook.com>
2023-02-15 17:26:35 +0100

    second commit

--- automl_baseline.py
+++ automl_baseline.py
@@ -1,7 +1,8 @@
 from ludwig.api import LudwigModel
 
-def one_line_model(train_dataset_path: str, config_path: str, test_dataset_path: str):
+
+def one_line_model(dataset_path: str, config_path: str, test_dataset_path: str):
     model = LudwigModel(config=config_path)
-    results = model.train(dataset=train_dataset_path)
+    results = model.train(dataset=dataset_path)
     eval_stats, _, _ = model.evaluate(dataset=test_dataset_path)
-    return eval_stats, results
+    return model, eval_stats, results
